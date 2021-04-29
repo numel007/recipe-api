6cd1a423dab41a7804a1573043453cdc4ba97c3b
Jackie <taekwondo.chan@gmail.com>
2021-04-28 21:26:30 -0700

    add post recipe route

--- src/routes/recipe.js
+++ src/routes/recipe.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../models/user')
 const Recipe = require('../models/recipe')
 
 // Get all recipes
@@ -15,9 +16,25 @@ router.get('/', (req, res) => {
 
 // POST new recipe
 router.post('/', (req, res) => {
-    let newRecipe = new Recipe(req.body);
+    let recipe = new Recipe(req.body);
 
-    newRecipe.save()
+    recipe.save()
+    .then( recipe => {
+        return User.findById(recipe.author)
+    })
+    .then( author => {
+        author.recipes.unshift(recipe)
+        return author.save()
+    })
+    .then( (author) => {
+        return res.json({
+            'message': `${recipe.title} added.`,
+            'author': author.username
+        })
+    })
+    .catch( err => {
+        throw err.message
+    })
 })
 
 module.exports = router;
