03b41fa928679429efc6a2c2355aea88eb68b18d
Jackie <taekwondo.chan@gmail.com>
2021-04-28 18:32:55 -0700

    add recipe model

---
+++ models/recipe.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const RecipeSchema = new Schema({
+    title: {type: String, required: true},
+    ingredients: {type: String, required: true},
+    method: {type: String, required: true},
+    // author: {type: Schema.Types.ObjectId, ref: 'User', required: true} // 'User' points at connected user model
+})
+
+const Recipe = mongoose.model('Recipe', RecipeSchema)
+
+module.exports = Recipe
