f6d773ba5d922831d2bae301399202df66532afc
Jackie <taekwondo.chan@gmail.com>
2021-04-28 19:23:04 -0700

    actually activate user routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const recipeRoutes = require('./recipe.js')
-// const userRoutes = require('./user.js)
+const userRoutes = require('./user.js')
 
 const router = express.Router()
 
-// Set default recipe routes to /recipe
 router.use('./recipe', recipeRoutes)
 router.use('./user', userRoutes)
 
