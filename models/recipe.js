66d35fd4135f49cf0aa2f0a11dda205958a09aee
Jackie <taekwondo.chan@gmail.com>
2021-04-28 19:22:04 -0700

    add author attribute to Recipe model

--- models/recipe.js
+++ models/recipe.js
@@ -5,7 +5,7 @@ const RecipeSchema = new Schema({
     title: {type: String, required: true},
     ingredients: {type: String, required: true},
     method: {type: String, required: true},
-    // author: {type: Schema.Types.ObjectId, ref: 'User', required: true} // 'User' points at connected user model
+    author: {type: Schema.Types.ObjectId, ref: 'User', required: true} // 'User' points at connected User model
 })
 
 const Recipe = mongoose.model('Recipe', RecipeSchema)
