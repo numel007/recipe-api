2d35551cb9fb7b80d55574be16cd8e1b059d3981
Jackie <taekwondo.chan@gmail.com>
2021-04-28 18:24:10 -0700

    server.js setup

---
+++ src/server.js
@@ -0,0 +1,17 @@
+require('dotenv/config')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+// Set up database
+require('./config/db-setup.js')
+
+// Routes
+const router = require('./routes/index.js')
+app.use(router)
+
+app.listen(process.env.PORT, () => {
+    console.log(`Recipe api listening on port ${process.env.PORT}`)
+})
