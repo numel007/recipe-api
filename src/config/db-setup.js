9f7603f631385c0d3c32591b2be6353e463b8d32
Jackie <taekwondo.chan@gmail.com>
2021-05-11 16:00:08 -0700

    deployment changes

--- src/config/db-setup.js
+++ src/config/db-setup.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
-const mongoUri = process.env.MONGODB_URI || "recipe-api";
+const mongoUri = process.env.MONGODB_URI;
 mongoose.set("useUnifiedTopology", true);
 mongoose.set("useFindAndModify", false);
 mongoose.connect(mongoUri, { useNewUrlParser: true });
 
-mongoose.connection.on("error", () => {
+mongoose.connection.on("error", (err) => {
+  console.log(err);
   throw new Error(`Could not connect to ${mongoUri}`);
 });
 
